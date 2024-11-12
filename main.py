b35e53403478d429dfc314a68c5b2ce7a8b82949
Mark Morcos <mark.yehia@gmail.com>
2024-11-12 22:42:50 +0100

    feat: apply formatters and save output to separate csv for each table

--- main.py
+++ main.py
@@ -2,15 +2,24 @@ import pandas as pd
 import functions
 
 def main():
-    df = pd.read_csv("dataset.csv")
+    df = pd.read_csv("./data/raw/dataset.csv")
 
-    df = functions.format_job_title(df)
-    df = functions.format_job_category(df)
-    df = functions.format_employment_type(df)
-    df = functions.format_experiences_level(df)
-    df = functions.format_employee_residency(df)
+    formatters = [
+        functions.format_employee_residence,
+        functions.format_employment_type,
+        functions.format_job_title,
+        functions.format_job_category,
+    ]
+    for formatter in formatters:
+        print(f"Running {formatter.__name__}... ", end="")
+        df = formatter(df)
+        print(f"Done ✅")
 
-    print(df.head())
+    columns = ["employee_residence", "work_setting", "employment_type", "job_title", "job_category"]
+    for column in columns:
+        print(f"Saving {column}... ", end="")
+        functions.save_unique_values(df, column, column + ".csv")
+        print(f"Done ✅")
 
 if __name__ == "__main__":
     main()
